0a46f30cde4c3bc998a5477d5032f0637e50a13b
Isaiah See <Isaiahsosee@gmail.com>
2026-02-19 14:55:09 +0800

    music bahay kubo
    kuba na ako

---
+++ Assets/C2_Bahay Kubo/Rhythm Game Tutorial/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public BeastScroller theBS;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+
+        }
+    }
+}
