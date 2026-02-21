b5ad02f1b4a5b22306c59ae168f619283a38359c
Pumpkid <Cmatyasin17@gmail.com>
2026-02-22 03:23:23 +0800

    GG

--- Assets/C2_Bahay Kubo/Script_Bahay Kubo/GameManager.cs
+++ Assets/C2_Bahay Kubo/Script_Bahay Kubo/GameManager.cs
@@ -1,65 +1,65 @@
-using UnityEngine;
-using UnityEngine.UI;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+//public class GameManager : MonoBehaviour
+//{
 
-    public AudioSource theMusic;
+//    public AudioSource theMusic;
 
-    public bool startPlaying;
+//    public bool startPlaying;
 
-    public BeastScroller theBS;
+//    public BeastScroller theBS;
 
-    public static GameManager instance;
+//    public static GameManager instance;
 
-    public int currentScore;
-    public int scorePerNote = 100;
+//    public int currentScore;
+//    public int scorePerNote = 100;
 
-    public int currentMultiplier;
-    public int multiplierTracker;
-    public int[] multiplierThresholds;
-
-
-    public Text scoreText;
-    public Text multiText;
+//    public int currentMultiplier;
+//    public int multiplierTracker;
+//    public int[] multiplierThresholds;
 
 
+//    public Text scoreText;
+//    public Text multiText;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        instance = this;
 
-        scoreText.text = "Score: 0 ";
-        currentMultiplier = 1;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!startPlaying)
-        {
-            if (Input.anyKeyDown)
-            {
-                startPlaying = true;
-                theBS.hasStarted = true;
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        instance = this;
 
-                theMusic.Play();
-            }
+//        scoreText.text = "Score: 0 ";
+//        currentMultiplier = 1;
+//    }
 
-        }
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (!startPlaying)
+//        {
+//            if (Input.anyKeyDown)
+//            {
+//                startPlaying = true;
+//                theBS.hasStarted = true;
 
-    public void NoteHit()
-    {
-        Debug.Log("Hit on time");
+//                theMusic.Play();
+//            }
 
-        currentScore += scorePerNote;
-        scoreText.text = "Score: " + currentScore * currentMultiplier;
-    }
+//        }
+//    }
 
-    public void NoteMissed()
-    {
-        Debug.Log("Missed Note");
-    }
-}
+//    public void NoteHit()
+//    {
+//        Debug.Log("Hit on time");
+
+//        currentScore += scorePerNote;
+//        scoreText.text = "Score: " + currentScore * currentMultiplier;
+//    }
+
+//    public void NoteMissed()
+//    {
+//        Debug.Log("Missed Note");
+//    }
+//}
